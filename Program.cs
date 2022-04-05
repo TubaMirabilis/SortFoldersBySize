621606d3e90fc28c8b0e0395aced60010224ad39
Ben Green <bkingsleygreen@gmail.com>
2022-04-05 11:11:26 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿List<NameSizePair> pairs = new List<NameSizePair>();
+foreach (var item in new DirectoryInfo(@"E:\X-Plane 11\Custom Scenery").GetDirectories())
+{
+    pairs.Add(new NameSizePair
+    {
+        Name = item.Name,
+        Size = DirSize(item)
+    });
+}
+var ordered = pairs.OrderBy(p => p.Size);
+foreach(var p in ordered)
+{
+    Console.WriteLine($"{p.Name}, {p.Size}");
+}
+long DirSize(DirectoryInfo d) 
+{    
+    long size = 0;    
+    // Add file sizes.
+    FileInfo[] fis = d.GetFiles();
+    foreach (FileInfo fi in fis) 
+    {      
+        size += fi.Length;    
+    }
+    // Add subdirectory sizes.
+    DirectoryInfo[] dis = d.GetDirectories();
+    foreach (DirectoryInfo di in dis) 
+    {
+        size += DirSize(di);   
+    }
+    return size;  
+}
+
+public class NameSizePair
+{
+    public string? Name { get; set; }
+    public long Size { get; set; }
+}
